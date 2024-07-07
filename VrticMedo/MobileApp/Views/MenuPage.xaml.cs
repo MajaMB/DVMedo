634cb39b86c2c91cd3e9a9530ffcadb08786cb04
MajaMB <maja.m.barukcic@gmail.com>
2024-07-07 11:48:44 +0200

    Set up project

---
+++ VrticMedo/MobileApp/Views/MenuPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileApp.Views;
+
+public partial class MenuPage : ContentPage
+{
+	public MenuPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ VrticMedo/MobileApp/Views/NewsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MobileApp.Views;
+
+public partial class NewsPage : ContentPage
+{
+	public NewsPage()
+	{
+		InitializeComponent();
+	}
+}
