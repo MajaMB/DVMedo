685d83170e2a694adee587a7e4891fbe3670882c
Maja <maja.m.barukcic@gmail.com>
2024-10-11 21:37:21 +0200

    upisi page added

--- kindedo-page/dev/upisi.php
+++ kindedo-page/dev/upisi.php
@@ -1,7 +1,92 @@
-<?php include('components/header.php'); ?>
-<main>
-    Upisi stranica
-</main>
+<?php 
+    include('components/header.php'); 
+    include('components/breadcrumb-area.php');
+    echo renderBreadcrumbArea('Upisi djeteta');
 
+?>
+<main>
+    <section class="bd-program-details-widget pt-120 pb-30">
+        <div class="container">
+            <div class="row">
+                <div class="col mb-20">
+                    <div class="bd-program-details-widget-content theme-bg-6 wow fadeInRight" data-wow-duration="1s"
+                        data-wow-delay=".3s">
+                        <p class="mb-25">
+                            Dječji vrtić "Medo" objavljuje javni natječaj za upis djece u pedagošku godinu 2024./2025. 
+                        </p>
+                        <p class="mb-25 fw-bold">
+                            Zahtjev za upis djeteta roditelj/skrbnik djeteta može, uz prethodnu telefonsku najavu, podnijeti u tajništvu vrtića.
+                        </p>
+                        <p class="mb-25">
+                            Sve dodatne informacije možete dobiti na info@vrtic-medo.hr, ili na broj telefona 092/4456-554.   
+                        </p>
+                        <p class="mb-25">
+                            Po zaprimljenom Zahtjevu, stručna služba vrtića će kontaktirati sve zainteresirane roditelje radi inicijalnih razgovora.
+                        </p>
+                        <p class="mb-25">
+                            Nakon inicijalnog razgovora, Upravno vijeće vrtića će razmatrati sve primljene zahtjeve nakon čega će se roditelji pozivati na potpisivanje ugovora do popunjavanja kapaciteta.  
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bd-program-details-widget pb-30">
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mb-50">
+                <div class="bd-program-details-widget-content theme-bg-6 wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                    <h3 class="bd-program-details-widget-title mb-20">Potrebna dokumentacija:</h3>
+                    <ul class="ul1 ml-25">
+                        <li class="li2"><a href="dokumenti/zahtjev-za-upis.pdf">Zahtjev za upis djeteta u vrtić (Preuzmi)</a></li>
+                        <li class="li2"><a href="http://vrtic-cupko.hr/dokumenti/obrasci_pvdv/">Zahtjev za sufinanciranje (Preuzmi)</a></li>
+                        <li class="li2"><a href="http://vrtic-cupko.hr/dokumenti/inicijalni-upitnik-2/">ispunjen Inicijalni upitnik (Preuzmi)</a></li>
+                        <li class="li2">Preslika rodnog lista ili izvatka iz Matice rođenih</li>
+                        <li class="li2">Preslike osobnih iskaznica roditelja/skrbnika</li>
+                        <li class="li2">Potvrda o prebivalištu djeteta i svih članova kućanstva</li>
+                        <li class="li2">Potvrde o svim primanjima u kućanstvu</li>
+                        <li class="li2">Preslika zdravstvene iskaznice djeteta</li>
+                        <li class="li2">Preslika kartona cijepljenja djeteta</li>
+                        <li class="li1">Potvrda liječnika-pedijatra o obavljenom sistematskom pregledu djeteta prije polaska u vrtić</li>
+                        <li><a href="http://vrtic-cupko.hr/wp-content/uploads/2018/07/Privola-o-osobnim-podacima-roditelji-1.pdf">Ispunjena Privola za osobne podatke roditelja (Preuzmi)</a></li>
+                        <li class="li1"><a href="http://vrtic-cupko.hr/dokumenti/dv-cupko-izjava-dovodenje-odvodenje-djece/">Izjava o dovođenju-odvođenju djeteta u vrtić (Preuzmi)</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6 mb-50">
+                <div class="bd-program-details-widget-content theme-bg-6 wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                    <h3 class="bd-program-details-widget-title mb-20">Bitni dokumenti</h3>
+                    <ul class="ul1 ml-25">
+                        <li class="li2"><a href="http://vrtic-cupko.hr/dokumenti/obavijest_pvdv1/">Sufinanciranje privatnih i vjerskih dječjih vrtića u 2024 (Preuzmi)</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="bd-program-details-widget pb-50">
+        <div class="container">
+            <div class="row">
+                <div class="col mb-20">
+                    <div class="bd-program-details-widget-content theme-bg-6 wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">
+                        <p class="mb-25">
+                            Zahtjev za ispis djeteta iz Dječjeg vrtića Vrapčić roditelj/skrbnik podnosi osobno u Upravu Dječjeg vrtića Medo 
+                            minimalno 15 dana prije željenog datuma ispisa, obavezno uz prethodnu telefonsku najavu.
+                        </p>
+                        <p class="mb-25 fw-bold">
+                            Ispisnica djeteta iz Dječjeg vrtića Vrapčić roditelju/skrbniku može biti izdana nakon izvršene provjere o podmirenosti financijskih obveza prema Vrtiću.
+                        </p>
+                        <ul class="ul1 ml-25">
+                            <li class="li2"><a href="https://vrapcic-djecji-vrtic.hr/wp-content/uploads/2020/05/Zahtjev-za-ispis-iz-Dje%C4%8Djeg-vrti%C4%87a-Vrap%C4%8Di%C4%87.pdf">Zahtjev za ispis (Preuzmi)</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
 
 <?php include('components/footer.php'); ?>
